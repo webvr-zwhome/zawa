2f0b0eecb3ea3ddcd415b786e341b9183518cceb
ZXQ-GIGI <snailzhao1995@163.com>
2018-05-20 02:17:03 +0800

    add gamepad

--- zawa/src/native_components/GamePad/index.js
+++ zawa/src/native_components/GamePad/index.js
@@ -2,15 +2,16 @@ import {Module} from "react-vr-web";
 import * as  THREE from 'three';
 
 export default class GamePad extends Module {
-    constructor(scene){
+    constructor(){
+        super('GamePad');
         // super('Fog');
         // this._scene = scene;
         // this._fog = new THREE.FogExp2( 0xefd1b5, 0.04);
         // this._fog = new THREE.Fog( 0xefd1b5, 0.005, 10);
-        const gamePad = window.navigator.getGamepads();
-        this.HapticActuators  = gamePad[1].hapticActuators;
+        const gamePad = navigator.getGamepads();
+        // this.HapticActuators  = gamePad[1].hapticActuators;
         this.init();
-        console.log(this.HapticActuators);
+        console.log(gamePad);
     }
 
     init() {
