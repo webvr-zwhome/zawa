5c54dc5c3fcd617ba2df904d29e0119d6f766157
ZXQ-GIGI <snailzhao1995@163.com>
2018-05-20 05:38:52 +0800

    change gamepad

--- zawa/src/native_components/GamePad/index.js
+++ zawa/src/native_components/GamePad/index.js
@@ -8,10 +8,15 @@ export default class GamePad extends Module {
         // this._scene = scene;
         // this._fog = new THREE.FogExp2( 0xefd1b5, 0.04);
         // this._fog = new THREE.Fog( 0xefd1b5, 0.005, 10);
-        const gamePad = navigator.getGamepads();
+        // const gamePad = navigator.getGamepads();
         // this.HapticActuators  = gamePad[1].hapticActuators;
+        window.ongamepadconnected = function(e) {
+            // All buttons and axes values can be accessed through
+            // let gamepad =  e.gamepad;
+            console.log('gamePad: ');
+          };
+        console.log('gamePad: ');
         this.init();
-        console.log(gamePad);
     }
 
     init() {
