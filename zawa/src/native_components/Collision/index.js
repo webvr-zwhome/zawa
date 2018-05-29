8d9f82439526dfce51b63761e9d12c1941ca10e4
ZXQ-GIGI <snailzhao1995@163.com>
2018-05-29 17:14:15 +0800

    change stone's position

--- zawa/src/native_components/Collision/index.js
+++ zawa/src/native_components/Collision/index.js
@@ -11,16 +11,16 @@ export default class Collision extends Module {
     init() {
         // this._scene.fog = this._fog;
         this.collisionList = [
-            [0.5, 3, -1.1],
-            [0, 3, -2],
-            [-0.9, 3, -2.5],
-            [-1.5, 3, -4.0],
-            [-2, 3, -4.8],
-            [-2.5, 3, -6.4],
-            [-3.0, 3, -8.2],
-            [-3.5, 3, -8.9],
-            [-4.5, 3, -10.0],
-            [-5.5, 3, -11]
+            [-1.5, 3, -1.3],
+            [-3, 3, -4],
+            [-5.7, 3, -5.5],
+            [-7.5, 3, -10],
+            [-8.9, 3, -12.2],
+            [-10.5, 3, -17],
+            [-12, 3, -22.5],
+            [-13.3, 3, -24.5],
+            [-16.3, 3, -27.9],
+            [-19.3, 3, -30.8]
         ]
     }
 
@@ -31,8 +31,8 @@ export default class Collision extends Module {
             // if((colPos[1]+1 > position[1]) && (Math.pow(colPos[0]-position[0], 2) + Math.pow(colPos[1]-position[1], 2) + Math.pow(colPos[2]-position[2], 4)) <= 2 ){
             //     isCollision = true;
             // }
-            // if((colPos[1] + 1  < position[1]) && (colPos[0]-0.5 < position[0]) && (position[0] < colPos[0]+0.5) && (colPos[2] - 0.5 < position[2]) && (position[2] < colPos[2] + 0.5)){
-            if(colPos[1] + 1  < position[1]){
+            if(( position[1] > colPos[1] )&&( position[1] < colPos[1]+2 ) && (colPos[0]-1 < position[0]) && (position[0] < colPos[0] + 1) && (colPos[2] - 1 < position[2]) && (position[2] < colPos[2] + 1)){
+            // if(colPos[1] + 1  < position[1]){
                 isCollision = true;
                 indexCol = index;
                 console.log('collistion sucess')
